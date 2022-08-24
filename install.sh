cf50f3aae60a33d7002ae4974a9d7ee5faca2784
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2022-08-24 14:01:35 +0000

    loading nvm

--- install.sh
+++ install.sh
@@ -20,6 +20,11 @@ systemctl enable spastaeriso
 cd ../..
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install 13
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 cd spastaeriso/app
 npm install
 npm run build
