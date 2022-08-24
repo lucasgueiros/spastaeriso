110ab79299ca481d0bf777a946bd3c008996a6a1
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2022-08-24 10:16:53 -0300

    biscoitos e lucas para spastaeriso

---
+++ install.sh
@@ -0,0 +1,31 @@
+apt-get update
+apt-get install git maven apache2 openjdk-11-jdk postgresql-10
+
+git clone https://github.com/lucasgueiros/spastaeriso
+cd spastaeriso
+git checkout tags/v1.0 -b master
+
+mkdir -p /opt/spastaeriso
+cp db/init/create.sql /opt/spastaeriso/initdb.sql
+chmod 500 /opt/spastaeriso/initdb.sql
+su postgres --command "psql -c \"create role spastaeriso login password 'spastaeriso'\""
+su postgres --command "psql -c \"create database spastaeriso with owner spastaeriso\""
+su postgres --command "psql -d spastaeriso -f /opt/spastaeriso/initdb.sql"
+
+cd core
+mvn install
+cd ../api
+mvn package
+cp target/spastaeriso-api-1.0.0.jar ~/spastaeriso-api.jar
+cp spastaeriso.service /etc/systemd/system/
+service spastaeriso start
+systemctl enable spastaeriso
+
+cd ../..
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install 13
+cd spastaeriso/app
+npm install
+npm run build
+rm -R /var/www/html/*
+cp -R build/* /var/www/html/
